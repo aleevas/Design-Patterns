a8ae8e5cd927d4387003497f65ed75f550a37bf1
Alexey Aleev <aleevas@gmail.com>
2019-06-25 13:14:21 +0300

    Example of simple extend of a class with the Decoration Pattern

--- TheDecorator/example.php
+++ TheDecorator/example.php
@@ -2,12 +2,18 @@
 
 interface CarService {
     public function getCoast();
+    public function getDescription();
 }
 
 class BasicInspection implements CarService {
     public function getCoast(){
         return 19;
     }
+
+    public function getDescription(){
+        return 'Basic inspection';
+    }
+
 }
 
 class OilChange implements CarService{
@@ -18,6 +24,10 @@ class OilChange implements CarService{
     }
     public function getCoast(){
         return 19 + $this->carService->getCoast();
+    }
+
+    public function getDescription(){
+        return $this->carService->getDescription() .  ', and Oil change';
     }
 }
 
@@ -30,6 +40,14 @@ class TairRotation implements CarService{
     public function getCoast(){
         return 10 + $this->carService->getCoast();
     }
+
+    public function getDescription(){
+        return $this->carService->getDescription() .  ', and tair rotation';
+    }
 }
 
 echo (new TairRotation(new OilChange(new BasicInspection())))->getCost();
+
+$service = new TairRotation(new BasicInspection);
+
+echo $service->getDescription();
