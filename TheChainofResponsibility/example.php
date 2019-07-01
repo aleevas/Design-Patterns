622f51d33d62257573590d53cb72d5a845ae8f9f
Alexey Aleev <aleevas@gmail.com>
2019-07-01 13:09:31 +0300

    added code refactor according to the chain of responsibility pattern

--- TheChainofResponsibility/example.php
+++ TheChainofResponsibility/example.php
@@ -1,33 +1,63 @@
 <?php
 
-class Locks {
+abstract class HomeChecker {
+    protected $successor;
+
+    public abstract function check(HomeStatus $home);
+
+    public function succedWith(HomeChecker $successor) {
+        $this->successor = $successor;
+    }
+    public function next(HomeStatus $home) {
+        if ($this->successor) {
+            $this->successor->check($home);
+        }
+    }
+}
+
+class Locks extends HomeChecker {
     public function check(HomeStatus $home){
         
         if (! $home->locked) {
             throw new Exception('The door not locked!');
         }
-        // Needs to transfer our status for all next options(Lights, Alarm).
-        // Let use The Chain Responsibility Pattern    
-
+        // Let's call next check.   
+        $this->next($home);
     }
 }
 
-class Lights {
-
+class Lights extends HomeChecker {
+    public function check(HomeStatus $home){
+        
+        if (! $home->lightsOff) {
+            throw new Exception('Turn lights on!');
+        }
+        // Let's call next check.   
+        $this->next($home);
+    }
 }
 
-class Alarm {
-
+class Alarm extends HomeChecker {
+    public function check(HomeStatus $home){
+        
+        if (! $home->alarmOn) {
+            throw new Exception('Turn alarm on!');
+        }
+        // Let's call next check.
+        $this->next($home);
+    }
 }
 
 class HomeStatus {
     public $alarmOn = true;
     public $locked = true;
-    public $lightsOff = true;
+    public $lightsOff = true; // for get an exception, just set it to false
 }
 
 $locks = new Locks;
+$lights = new Lights;
+$alarm = new Alarm;
 
-$status = new HomeStatus;
-
-$locks->check($status);
+$locks->succedWith($lights);
+$lights->succedWith($alarm);
+$locks->check(new HomeStatus);
